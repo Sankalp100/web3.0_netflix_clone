8c743bb2f2ad152bc880b3ac89a467bf17db8c19
Sankalp100 <sankalpgour123@gmail.com>
2022-11-30 02:39:40 +0530

    movie modal page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 
 import { Logo } from "../images/Netflix";
-import { ConnectButton, Icon, TabList, Tab, Button } from "web3uikit";
+import { ConnectButton, Icon, TabList, Tab, Button, Modal } from "web3uikit";
 import { movies } from "../helpers/library";
 
 const Home = () => {
+  const [visible, setVisible] = useState(false);
+  const [selectedFilms, setSelectedFilms] = useState();
+
   return (
     <>
       <div className="logo">
@@ -42,13 +45,68 @@ const Home = () => {
             <div className="thumbs">
               {movies &&
                 movies.map((e) => {
-                  return <img src={e.Thumnbnail} className="thumbnail"></img>;
+                  return (
+                    <img
+                      src={e.Thumnbnail}
+                      className="thumbnail"
+                      onClick={() => {
+                        setSelectedFilms(e);
+                        setVisible(true);
+                      }}
+                    ></img>
+                  );
                 })}
             </div>
           </Tab>
           <Tab tabKey={2} tabName={"Series"} isDisabled={true}></Tab>
           <Tab tabKey={3} tabName={"MyList"}></Tab>
         </TabList>
+        {selectedFilms && (
+          <div className="modal">
+            <Modal
+              onCloseButtonPressed={() => {
+                setVisible(false);
+              }}
+              isVisible={visible}
+              hasFooter={false}
+              width="1000px"
+            >
+              <div className="modalContent">
+                <img src={selectedFilms.Scene} className="modalImg" />
+                <img src={selectedFilms.Logo} className="modalLogo" />
+                <div className="modalPlayButton">
+                  <Button
+                    icon="chevronRightX2"
+                    text="Play"
+                    theme="secondary"
+                    type="button"
+                  />
+                  <Button
+                    icon="plus"
+                    text="Add to My List"
+                    theme="translucent"
+                    type="button"
+                  />
+                </div>
+                <div className="movieInfo">
+                  <div className="description">
+                    <div className="details">
+                      <span>{selectedFilms.Year}</span>
+                      <span>{selectedFilms.Duration}</span>
+                    </div>
+                    {selectedFilms.Description}
+                  </div>
+                  <div className="detailedInfo">
+                    Genre:<span className="deets">{selectedFilms.Genre}</span>
+                    <br></br>
+                    Actors:
+                    <span className="deets">{selectedFilms.Actors}</span>
+                  </div>
+                </div>
+              </div>
+            </Modal>
+          </div>
+        )}
       </div>
     </>
   );
